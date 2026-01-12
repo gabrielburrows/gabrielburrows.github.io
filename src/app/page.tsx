01dcac36fa032a0739147724baea234bb67074fd
x-Yusu <gabybaby695@gmail.com>
2026-01-12 10:50:54 -0500

    Rename Projects component and update import
    Renamed Projects.tsx to ProjectsTemp.tsx and updated the import in page.tsx to reflect the new filename.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Education from "../components/Education";
-import Projects from "../components/Projects";
+import Projects from "../components/ProjectsTemp";
 import Stack from "../components/Stack";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
